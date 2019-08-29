48ad3fd12a26fb9b0e8c72789e3f16e9db4bbf0d
robinl3680 <robinl3680@gmail.com>
2019-08-29 13:40:08 +0530

    Assignment 2 added

---
+++ Assignment2/Assignment2/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+namespace Assignment2
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Enter the string");
+            string sentence = Console.ReadLine();
+            SortedList<string, int> result= WordsCount.CountWords(sentence);
+            foreach(KeyValuePair<string,int> item in result)
+            {
+                Console.WriteLine($"{item.Key} : {item.Value}");
+            }
+        }
+    }
+}
---
+++ Assignment2/Assignment2/WordsCount.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Assignment2
+{
+    class WordsCount
+    {
+        public static SortedList<string,int> CountWords(string sentence)
+        {
+            int index = 0;
+            sentence = sentence.ToLower();
+            char[] delimeters = new char[sentence.Length];
+            for(int i = 0; i < sentence.Length; i++)
+            {
+                if (!char.IsLetterOrDigit(sentence[i]) && sentence[i] != '@')
+                    delimeters[index++] = sentence[i];
+            }
+
+            string[] words = sentence.Split(delimeters,StringSplitOptions.RemoveEmptyEntries);
+            SortedList<string, int> wordsDictionary = new SortedList<string, int>();
+            foreach(string items in words)
+            {
+                if (wordsDictionary.ContainsKey(items))
+                    wordsDictionary[items]++;
+                else
+                    wordsDictionary[items] = 1;
+            }
+            return wordsDictionary;
+        }
+   }
+}

581ca32bb20c7a64c54c6b96ef7d5a79e8ae4883
robinl3680 <robinl3680@gmail.com>
2019-08-29 14:55:23 +0530

    New

--- SortStringUsingLength/SortStringUsingLength/Program.cs
+++ SortStringUsingLength/SortStringUsingLength/Program.cs
@@ -8,8 +8,33 @@ namespace SortStringUsingLength
 {
     class Program
     {
+        static void SortStrings(ref List<string> strings)
+        {
+            StringSort sorter = new StringSort();
+            strings.Sort(sorter);
+        }
+        static void PrintList(List<string> strings)
+        {
+            foreach (string items in strings)
+                Console.WriteLine(items);
+        }
         static void Main(string[] args)
         {
+            List<string> strings = new List<string>();
+            while(true)
+            {
+                Console.WriteLine("Enter the string");
+                string s1 = Console.ReadLine();
+                strings.Add(s1);
+                Console.WriteLine("Want to break ? y : yes");
+                string y = Console.ReadLine();
+                if (y == "y")
+                    break;
+            }
+            SortStrings(ref strings);
+            PrintList(strings);
+            
+            
 
         }
     }
---
+++ SortStringUsingLength/SortStringUsingLength/StringSort.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SortStringUsingLength
+{
+    public class StringSort : IComparer<string>
+    {
+        public StringSort()
+        {
+
+        }
+        public int Compare(string x, string y)
+        {
+            if (x.Length > y.Length)
+                return 1;
+            else if (x.Length < y.Length)
+                return -1;
+            else
+                return 0;
+        }
+    }
+}

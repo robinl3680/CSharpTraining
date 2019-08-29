de52329b1e0ff90bcda2eef4c9db056d749be165
robinl3680 <robinl3680@gmail.com>
2019-08-29 13:31:01 +0530

    New project

---
+++ Asignment1/Asignment1/CompositeFinder.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Asignment1
+{
+    public class CompositeFinder
+    {
+        public CompositeFinder()
+        {
+
+        }
+        public static string Findmatch(string word, string[] sentence)
+        {
+            if (Array.Exists(sentence, element => element == word))
+                  return word;
+            else
+                return null;
+        }
+        public static List<string> Combination(string[] sentence)
+        {
+            List<string> combined = new List<string>();
+            
+            
+            foreach(string item in sentence)
+            {
+                foreach(string item1 in sentence)
+                {
+                    string result = Findmatch(item + item1, sentence);
+                    if(result != null)
+                    {
+                        combined.Add($"{item},{item1},{result}");
+                    }
+                }
+            }
+            return combined;
+        }
+
+       
+
+        public static List<string> FindCompositeWordds(string sentence)
+        {
+            string[] words = sentence.Split(',');
+            List<string> result = Combination(words);
+            return result;
+
+           
+        }
+    }
+}
---
+++ Asignment1/Asignment1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+namespace Asignment1
+{
+    class Program
+    {
+        static void Main()
+        {
+            
+            CompositeFinder composite = new CompositeFinder();
+            Console.WriteLine("Enter the string");
+            string sentence = Console.ReadLine();
+            List<string> result = CompositeFinder.FindCompositeWordds(sentence);
+            foreach(string item in result)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
